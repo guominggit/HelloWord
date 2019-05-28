8316d2c6d7670dfc82f1e0b9603d465512653c22
guominggit <19646200@qq.com>
2019-05-28 16:18:09 +0800

    zengjia

---
+++ src/Test/Scannar2.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Scannar2 {
+
+	public static void main(String[] args) {
+	
+
+		System.out.println("姓名：" + "郭明");
+		System.out.println("年龄：" + "23");
+		System.out.println("性别：" + "男");
+		System.out.println("兴趣：" + "打篮球");
+
+	}
+
+}
